8e4ad5e8e277700d5d815db0ce3395ea2bbec970
Ammar Hassan <homesuper7@gmail.com>
2024-06-04 15:55:53 +0500

    Added password generation functionality and better styling

---
+++ script.js
@@ -0,0 +1,41 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+const passwordContainer = document.querySelector('.password-form')
+
+let password = "";
+
+function generateRandomPassword() {
+    let passwordField = document.getElementById("output-form");
+
+    for (let i = 0; i < 15; i++) {
+        let char = characters[Math.floor(Math.random() * 91)];
+        password += char;
+    }
+
+    passwordField.value=password;
+}
+
+passwordContainer.addEventListener('click', () => {
+    navigator.clipboard.writeText(password).then(() => {
+        console.log("Password copied to clipboard");
+        const message = document.createElement('div');
+        message.textContent = 'Password copied to clipboard';
+        message.style.position = 'fixed';  // Make it positioned relative to the viewport
+        message.style.bottom = '35px';   // Position it 10px from the bottom
+        message.style.left = '50%';      // Center it horizontally
+        message.style.transform = 'translate(-50%)'; // Offset it horizontally by half its width
+        message.style.borderRadius = '12px';
+
+        message.style.backgroundColor = '#d6ffd6';
+        message.style.padding = '5px';
+        message.style.fontFamily = 'Inter', 'Times New Roman'; // Change font to Arial or any desired font
+
+        passwordContainer.appendChild(message);
+
+        setTimeout(() => {
+            message.remove(); 
+        }, 2000);
+    }, (err) => {
+        console.error("Failed to copy password:", err);
+    });
+});
